4fefb996c6e268d194d1c8ccef2e067694517efa
Moaaz Ahmed <imoaazahmed@gmail.com>
2024-01-19 19:04:04 +0400

    feat(providers): add basic providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./main.css";
+import { Providers } from "./providers.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<Suspense>
+			<BrowserRouter>
+				<Providers>
+					<App />
+				</Providers>
+			</BrowserRouter>
+		</Suspense>
+	</React.StrictMode>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
+};
