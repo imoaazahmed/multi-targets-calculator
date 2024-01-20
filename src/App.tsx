e7c8a0e376fe8615ab10c059756b04e87f737ab7
Moaaz Ahmed <imoaazahmed@gmail.com>
2024-01-20 22:30:55 +0400

    add copyrights and made by

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { CalculatorForm } from "./components/calculator-form";
 import { ThemeSwitch } from "./components/theme-switch";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { Link } from "./theme/components";
 
 function App() {
 	return (
-		<>
+		<main>
 			<Navbar position="static" maxWidth="full">
 				<NavbarBrand>
 					<h1 className="text-xl text-center font-bold">Total Profit Calculator</h1>
@@ -19,8 +20,17 @@ function App() {
 
 			<div className="container mx-auto my-8">
 				<CalculatorForm />
+
+				<div className="flex gap-unit-xs items-center justify-center mt-8">
+					<p>Made By 🇪🇬</p>
+					<Link href="https://www.linkedin.com/in/imoaazahmed/" isExternal color="foreground">
+						Moaaz
+					</Link>
+					<p>•</p>
+					<p>© {new Date().getFullYear()}</p>
+				</div>
 			</div>
-		</>
+		</main>
 	);
 }
 
